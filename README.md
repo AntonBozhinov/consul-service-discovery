bb1a942fb2908885e84cc3d5c8bb535bdd8ccd43
anton.bozhinov <anton.bozhinov@softwaregroup.com>
2017-10-01 14:25:42 +0300

    define nginx service and add readme info

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # consul-service-discovery
 Consul service diskovery lunch and learn talk
+
+## Installation
+    1. Install vagrant localy on your machine
+    2. git clone git@github.com:AntonBozhinov/consul-service-discovery.git
+    3. cd consul-service-discovery
+    4. vagrant up
--- setup/startup/web.sh
+++ setup/startup/web.sh
@@ -2,10 +2,30 @@
 
 echo 'STARTING CONSUL FOR NODE WEB'
 
+# Get the ip of the current machine
 ip=$(ifconfig eth1 | grep 'inet addr' | awk '{ print substr($2, 6) }')
 
+echo 'COPY SERVICE DEFINITIONS'
+sudo cp -a /vagrant/services/. /etc/consul.d
+
+# Start consul agent
 sudo nohup consul agent \ 
 -bind $ip \
 -config-file /vagrant/setup/agent/web.json \
 -config-dir /etc/consul.d \
 </dev/null &>/dev/null &
+
+
+echo 'STARTING THE NGINX SERVICE'
+
+# Add a simple html page to be served
+echo '<h1>Node: $(hostname), Ip: $ip</h1>' > /home/vagrant/ip.html
+
+# Run docker nginx image
+docker run -d \
+--name web \
+-p 8080:80 \
+--restart unless-stopped \
+-v /home/vagrant/ip.html:/usr/share/nginx/html/ip.html:ro \
+nginx
+

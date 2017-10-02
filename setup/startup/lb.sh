24703f72983fd166da22aa4544b3918576b0dbcc
anton.bozhinov <anton.bozhinov@softwaregroup.com>
2017-10-02 08:09:10 +0300

    add load balance config

--- setup/startup/lb.sh
+++ setup/startup/lb.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 echo 'STARTING CONSUL FOR LOAD BALANCER'
+
+echo "COPY SERVICE DEFINITIONS"
+sudo cp -a /vagrant/services/lb/. /etc/consul.d
+
 sudo nohup consul agent \
 -config-file /vagrant/setup/agent/lb.json \
 -config-dir /etc/consul.d \
--- setup/startup/web.sh
+++ setup/startup/web.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
 
-echo 'STARTING CONSUL FOR NODE WEB'
+echo "STARTING CONSUL FOR NODE WEB"
+
+
+echo "COPY SERVICE DEFINITIONS"
+sudo cp -a /vagrant/services/web/. /etc/consul.d
 
 # Get the ip of the current machine
-ip=$(ifconfig eth1 | grep 'inet addr' | awk '{ print substr($2, 6) }')
-
-echo 'COPY SERVICE DEFINITIONS'
-sudo cp -a /vagrant/services/. /etc/consul.d
+ip=$(ifconfig eth1 | grep "inet addr" | awk '{ print substr($2, 6) }')
 
 # Start consul agent
-sudo nohup consul agent \ 
+sudo nohup consul agent \
 -bind $ip \
 -config-file /vagrant/setup/agent/web.json \
 -config-dir /etc/consul.d \
 </dev/null &>/dev/null &
 
 
-echo 'STARTING THE NGINX SERVICE'
+echo "STARTING THE NGINX SERVICE"
 
 # Add a simple html page to be served
-echo '<h1>Node: $(hostname), Ip: $ip</h1>' > /home/vagrant/ip.html
+echo "<h1>Node: $(hostname), Ip: $ip</h1>" > /home/vagrant/ip.html
 
 # Run docker nginx image
 docker run -d \
